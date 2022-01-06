00c3cec86c6208bf9dfa5100b3fca98173551c69
harsha2 <harshakrishnak5@gmail.com>
2022-01-06 12:38:18 +0530

    Redirection Updated

--- server.js
+++ server.js
@@ -12,35 +12,22 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 const fs = require('fs');
 const { response } = require('express');
 
-
-
-
 var data1 = require('./data/users.json');
-
-
-
 
 const routes = require('./routes/routes.js')(app, fs);
 
-// set the view engine to ejs
-// app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page
+// index page renderring
 app.get('/', function(req, res) {
 
     fs.readFile('./data/users.json', function(err, data) {
         if (err) throw err;
         var data1 = JSON.parse(data);
 
-        // console.log(data)
     });
 
     var users = data1;
-    // console.log(data)
     var tagline = "User Info";
-
     res.render('pages/index', {
         users: users,
         tagline: tagline
@@ -55,27 +42,21 @@ app.get('/addUser', function(req, res) {
 });
 app.post('/processPost', urlencodedParser, function(req, res) {
 
-
     fs.readFile('./data/users.json', function(err, data) {
         if (err) throw err;
         data1 = JSON.parse(data);
 
-        // console.log(data)
     });
 
     var users = data1;
-    //console.log(JSON.stringify(data));
+
     var array = [];
     var count = 0;
-    // JSON.parse(data).forEach(function(item) {
-    //     // array.push(item);
-    //     console.log(item);
-    // });
 
     for (var i in data1) {
         array.push(data1[i]);
         count++;
-        console.log(i);
+
     }
 
     count = array.length + 1;
@@ -85,18 +66,23 @@ app.post('/processPost', urlencodedParser, function(req, res) {
         address: req.body.address,
         phone: req.body.phone
     };
-    // console.log(array);
+
     try {
         array.push(response);
-        console.log(array)
-        fs.writeFileSync('./data/users.json', JSON.stringify(array))
-            //file written successfully
+
+        fs.writeFileSync('./data/users.json', JSON.stringify(array));
+        // app.use('/', function(req, res) {
+        //     res.redirect(301, 'pages/index');
+        //     res.status(401).end();
+
+        // })
+
     } catch (err) {
         console.error(err)
     }
-    // console.log(newList);
-    res.end(JSON.stringify(response));
-    // console.log(req, "request");
+
+    res.redirect('/');
+
 });
 
 
@@ -110,18 +96,15 @@ app.get('/editUser/:id', function(req, res) {
         if (err) throw err;
         data1 = JSON.parse(data);
 
-        // console.log(data)
     });
 
     var users = data1;
-    // console.log(req.params.id);
+
     var id = req.params.id;
     for (const i in data1) {
         if (data1[i]["id"] == id) {
             userData = data1[i];
         }
-        // array.push(data[i]);
-        // console.log(data[i]);
     }
     res.render('pages/editUser', userData);
 });
@@ -134,7 +117,6 @@ app.post('/processPatch/:id', urlencodedParser, function(req, res) {
         if (err) throw err;
         data1 = JSON.parse(data);
 
-        // console.log(data)
     });
 
     var users = data1;
@@ -146,35 +128,26 @@ app.post('/processPatch/:id', urlencodedParser, function(req, res) {
         address: req.body.address,
         phone: req.body.phone
     };
-    // console.log(JSON.stringify(data));
-    var array = [];
-    // JSON.parse(data).forEach(function(item) {
-    //     // array.push(item);
-    //     console.log(item);
-    // });
 
+    var array = [];
 
     for (const i in data1) {
         if (data1[i]["id"] == id) {
-            var index = data1.indexOf(data1[i]);
-            //            data.splice(index, 1);
             array.push(response);
         } else
             array.push(data1[i]);
-        // console.log(data[i]);
     }
 
-    // console.log(array);
     try {
-
         fs.writeFileSync('./data/users.json', JSON.stringify(array))
             //file written successfully
     } catch (err) {
         console.error(err)
     }
-    // console.log(newList);
-    res.end(JSON.stringify(response));
-    // console.log(req, "request");
+
+    res.redirect('/');
+
+
 });
 
 // delete user
@@ -184,41 +157,26 @@ app.get('/processDelete/:id', urlencodedParser, function(req, res) {
         if (err) throw err;
         data1 = JSON.parse(data);
 
-        // console.log(data)
     });
 
     var users = data1;
-
     var id = req.params.id;
-
-    // console.log(JSON.stringify(data));
     var array = [];
-    // JSON.parse(data).forEach(function(item) {
-    //     // array.push(item);
-    //     console.log(item);
-    // });
-
-
     for (const i in data1) {
         if (data1[i]["id"] != id) {
-
             array.push(data1[i]);
         }
-
-        // console.log(data[i]);
     }
-
-    // console.log(array);
     try {
-
         fs.writeFileSync('./data/users.json', JSON.stringify(array))
             //file written successfully
     } catch (err) {
         console.error(err)
     }
-    // console.log(newList);
-    res.end(JSON.stringify({ "Message": "Successfully Deleted" }));
-    // console.log(req, "request");
+    // res.end(JSON.stringify({ "Message": "Successfully Deleted" }));
+    res.redirect('/');
+
+
 });
 
 // launch server
